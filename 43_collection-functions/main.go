9626ecb047ffea00d66205827b07b07481fb6d5c
Roy Wei <roywjy@gmail.com>
2018-09-24 23:26:11 +1000

    Add comments on collection-functions

--- 43_collection-functions/main.go
+++ 43_collection-functions/main.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// Index returns the first index of the target string t, or -1 if no match is found.
 func Index(vs []string, t string) int {
 	for i, v := range vs {
 		if v == t {
@@ -14,6 +15,7 @@ func Index(vs []string, t string) int {
 	return -1
 }
 
+// Include returns true if the target string t is in the slice.
 func Include(vs []string, t string) bool {
 	return Index(vs, t) >= 0
 }
